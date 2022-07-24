ca5d3859e4cd2586e29b3d88b45e160ec4540d54
Manux21 <Manux21@yandex.ru>
2022-07-25 00:46:42 +0300

    added onAnswerClick function

--- src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
+++ src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -7,6 +7,7 @@ const AnswerItem = props => {
   return (
     <li
       className={classes.AnswerItem}
+      onClick={() => props.onAnswerClick(props.answer.id)}
     >
       { props.answer.text }
     </li>
