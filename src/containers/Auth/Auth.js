e6ef4fe3a86f0741b7db4f1be5b2c385f274817e
Manux21 <Manux21@yandex.ru>
2022-08-05 23:57:54 +0300

    updated autorization component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
+import axios from 'axios'
 
 
 
@@ -39,11 +40,32 @@ export default class Auth extends Component {
     }
   }
 
-  loginHandler = () => {
-
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try{
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDQCBZ1Eaz8S9A9YdiA5_ecmEY5FAhjar4', authData)
+      console.log(response.data)
+    } catch(e){
+      console.log(e)
+    }
   }
 
-  registerHandler = () => {
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try{
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDQCBZ1Eaz8S9A9YdiA5_ecmEY5FAhjar4', authData)
+      console.log(response.data)
+    } catch(e){
+      console.log(e)
+    }
 
   }
 
